15115b954d27ca3dda99a43d29401cc0ea70c392
Igor Markiv <im@s4h.pl>
2024-01-23 10:55:39 +0100

    Add project files.

---
+++ webView/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace webView
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ webView/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace webView
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+		HttpClient httpClient = new HttpClient();
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			// Existing code to update counter
+		}
+
+		// Method to handle button clicks and update WebView
+		private async void OnButtonClicked(object sender, EventArgs e)
+		{
+			Button button = (Button)sender;
+			string url = button.CommandParameter.ToString(); // URL will be set as CommandParameter in XAML
+
+			try
+			{
+				string html = await httpClient.GetStringAsync(url);
+				MyWebView.Source = new HtmlWebViewSource { Html = html, BaseUrl = url };
+			}
+			catch (Exception ex)
+			{
+				// Handle exceptions, such as network errors
+				await DisplayAlert("Error", $"Failed to load content: {ex.Message}", "OK");
+			}
+		}
+
+
+	}
+
+}
